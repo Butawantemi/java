492f4fbcbcd720a3e2d7b6b23424c4d7251a3532
Butawantemi <japhetbuta@gmail.com>
2024-11-08 16:28:29 +0300

    java-basic

--- Test.java
+++ Test.java
@@ -1,8 +1,9 @@
 /*
  * This is a simple program that prints out the name and age of aperson
+ * printl() method is used to print the output with a newline at the end.
  * printf() method is used to format the output.
  * The %s(string) and %d(integer) are format specifiers that are used to format the output.
- * print() method is used to print the output without a newline at the end
+ * print() method is used to print the output without a newline at the end.
  * Author: Japhet
  * Date: 6th November 2024.
  */
@@ -10,9 +11,10 @@
 
 public class Test {
     public static void main(String[] args) {
-        String name = "Japhet";
+        String firstName = "Japhet";
+        String lastName = "Paul";
         int age = 22;
 
-        System.out.println("Hello, my name is  " + name + " and I am  " + age + " years old.");
+        System.out.println("Hello, my name is  " + firstName + " " + lastName + " and I am  " + age + " years old.");
     }
 }

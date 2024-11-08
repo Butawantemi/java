d6ba68983078f064096c38ffd421962e1bdd2ca4
Butawantemi <japhetbuta@gmail.com>
2024-11-08 16:26:04 +0300

    java-basic

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
 /*
  * This is a simple program that prints out the name and age of aperson
+ * printf() method is used to format the output.
+ * The %s(string) and %d(integer) are format specifiers that are used to format the output.
+ * print() method is used to print the output without a newline at the end
  * Author: Japhet
  * Date: 6th November 2024.
  */

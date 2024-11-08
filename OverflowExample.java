9c3a7a67909f73abb65c469067b5e2ecd0810c2a
Butawantemi <japhetbuta@gmail.com>
2024-11-08 10:24:42 +0300

    java-basic

--- OverflowExample.java
+++ OverflowExample.java
@@ -1,3 +1,11 @@
 public class OverflowExample {
-    
+    public static void main(String args[]) {
+	
+		int overFlow = 2147483647;
+		System.out.println("Overflow : "  + (overFlow + 1));         
+		
+		int underFlow = -2147483648;
+		System.out.println("Underflow : " + (underFlow - 1));
+     	
+ 	}
 }

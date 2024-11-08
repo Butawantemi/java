6c00591d74020e9c1efcd8ac81fb2d5d91e4992c
Butawantemi <japhetbuta@gmail.com>
2024-11-08 16:19:26 +0300

    java-basic

--- Test.java
+++ Test.java
@@ -1,3 +1,8 @@
 public class Test {
-    
+    public static void main(String[] args) {
+        String name = "Japhet";
+        int age = 22;
+
+        System.out.println("Hello, my name is  " + name + " and I am  " + age + " years old.");
+    }
 }

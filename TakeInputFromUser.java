aa466ee4e0cc1c82b26acf909eb997d84f68ed26
Butawantemi <japhetbuta@gmail.com>
2024-11-08 17:08:21 +0300

    java-basic

--- TakeInputFromUser.java
+++ TakeInputFromUser.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class TakeInputFromUser {
     public static void main(String arg[]) {
-        
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter your first name: ");
+      String firstName = sc.nextLine();
+
+      System.out.println("Enter your last name: ");
+      String lastName = sc.nextLine();
+
+      String fullName = firstName + "\t" + lastName;
+      System.out.println("Hello, my name is " + fullName);
     }
 }

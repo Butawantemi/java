bf62a76ac8df7fbce3b4147dfa9134d672bd0f3b
Butawantemi <japhetbuta@gmail.com>
2024-11-08 09:28:07 +0300

    java-basic

---
+++ Addition.java
@@ -0,0 +1,18 @@
+public class Addition {
+    public void add() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println(c);
+
+    }
+
+    public static void main(String args[]) {
+
+        Addition obj = new Addition();
+        obj.add();
+
+    }
+    
+}

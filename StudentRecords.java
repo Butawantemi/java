765fa4c65517f0b218ce2fb3e159454dbce0fedc
Butawantemi <japhetbuta@gmail.com>
2024-11-08 09:46:04 +0300

    java-basic

--- StudentRecords.java
+++ StudentRecords.java
@@ -9,5 +9,12 @@ class Student {
 }
 
 public class StudentRecords {
-    
+    public static void main(String args[]) {
+         Student obj = new Student();
+         obj.name = "Japhet";
+         obj.rollno = 1;
+
+         System.out.println(obj.name);
+         System.out.println(obj.rollno);
+    }
 }

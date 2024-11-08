fc1f49710d37fa0790ff6726dc70e824b426356b
Butawantemi <japhetbuta@gmail.com>
2024-11-08 10:13:55 +0300

    java-basic

--- ExplicitConversation.java
+++ ExplicitConversation.java
@@ -1,3 +1,11 @@
 public class ExplicitConversation {
-    
+    public static void main(String args[]) {
+
+		double d = 25.123;
+		int i = (int) d;
+		byte b = (byte) i;
+		System.out.println("Double value : " + d);
+		System.out.println("Int value : " + i);
+		System.out.println("Byte value : " + b);
+	}
 }

731c763a15f158407b14f9176bc01f5860887492
Butawantemi <japhetbuta@gmail.com>
2024-11-10 20:44:13 +0300

    java-basic

---
+++ JavaEnhancedForLoop.java
@@ -0,0 +1,8 @@
+public class JavaEnhancedForLoop {
+    public static void main(String[] args) {
+        String[] names = {"Jackline", "Jackson", "Japhet", "Josephat", "Josia", "Jovin"};
+    
+        for (String name: names) {
+            System.out.println(name);
+        }}
+}

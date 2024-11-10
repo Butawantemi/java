1f5ab97c325e581ba0d3f2d88988f9f58cf49059
Butawantemi <japhetbuta@gmail.com>
2024-11-10 14:08:18 +0300

    java-basic

--- JavaIfStatement.java
+++ JavaIfStatement.java
@@ -4,5 +4,12 @@
  */
 
 public class JavaIfStatement {
+    public static void main(String[] args) {
+        int a = 10;
+
+    if(a > 20) {
+        System.out.println("If block is executed");
+    }
+    }
     
 }

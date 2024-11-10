c0c22edc18a9fa7f045de0ebb669819b6c16b5e1
Butawantemi <japhetbuta@gmail.com>
2024-11-10 19:21:07 +0300

    java-basic

--- JavaDoWhileLoop.java
+++ JavaDoWhileLoop.java
@@ -1,6 +1,10 @@
 public class JavaDoWhileLoop {
     public static void main(String[] args) {
-        
+        int i = 1;
+        do {
+            System.out.println(i + "");
+            i++;
+        } while (i <= 10);
     }
     
 }

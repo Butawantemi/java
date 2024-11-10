e897c8b1d74e672dee60517b7114b03f60b02832
Butawantemi <japhetbuta@gmail.com>
2024-11-10 15:17:14 +0300

    java-basic

--- Solution2.java
+++ Solution2.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Solution2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        char inputUser = sc.next().charAt(0);
+        if (Character.isUpperCase(inputUser)) {
+            System.out.println(1);
+        } else if (Character.isLowerCase(inputUser)) {
+            System.out.println(0);
+        } else {
+            System.out.println(-1);
+        }
     }
 }

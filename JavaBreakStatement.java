2b654d811de4b5d25237ffc2b427cd2abdb6020b
Butawantemi <japhetbuta@gmail.com>
2024-11-10 20:53:19 +0300

    java-basic

--- JavaBreakStatement.java
+++ JavaBreakStatement.java
@@ -1,5 +1,10 @@
 public class JavaBreakStatement {
     public static void main(String[] args) {
-        
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }

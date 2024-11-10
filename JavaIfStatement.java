c00b827ce5c3053a1eeb22e5b18838db8c0787ae
Butawantemi <japhetbuta@gmail.com>
2024-11-10 14:09:04 +0300

    java-basic

--- JavaIfStatement.java
+++ JavaIfStatement.java
@@ -7,7 +7,7 @@ public class JavaIfStatement {
     public static void main(String[] args) {
         int a = 10;
 
-    if(a > 20) {
+    if(a < 20) {
         System.out.println("If block is executed");
     }
     }

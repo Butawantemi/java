84aee3cbd803f9e27012cf0c4776b1e6f81a8b30
Butawantemi <japhetbuta@gmail.com>
2024-11-10 14:19:05 +0300

    java-basic

--- JavaNestedIfStatement.java
+++ JavaNestedIfStatement.java
@@ -1,3 +1,16 @@
 public class JavaNestedIfStatement {
-    
+    public static void main(String[] args) {
+        int a = 20;
+        if(a == 20) {
+            if(a < 25) {
+                System.out.println("a is less than 25");
+            } 
+
+            if(a < 22) {
+                System.out.println("a is less than 22");
+            } else {
+                System.out.println("a is greater than 25");
+            }
+        }
+    }
 }

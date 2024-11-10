73d5848ea6998568cfe3afdc9d1f837a503a3668
Butawantemi <japhetbuta@gmail.com>
2024-11-10 21:05:20 +0300

    java-basic

--- JavaLabeledBreak.java
+++ JavaLabeledBreak.java
@@ -1,8 +1,14 @@
 public class JavaLabeledBreak {
     public static void main(String[] args) {
         boolean b = true;
-        first: {
+        First: {
             Second: {
+                Third: {
+                        System.out.println("Before the break statement");
+                        if (b) {
+                            break Second;
+                        }
+                }
                 System.out.println("this is the second block");
             }
             System.out.println("this is the first block");

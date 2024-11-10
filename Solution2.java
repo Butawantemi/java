9e2f8b88ed2cb1a3b871e2a712901c4490dad29f
Butawantemi <japhetbuta@gmail.com>
2024-11-10 15:41:57 +0300

    java-basic

--- Solution2.java
+++ Solution2.java
@@ -1,3 +1,19 @@
+/*
+ * Problem statement:Write a program that takes a character as input and prints either 1, 0, or -1 according to the following rules.
+ * 1, if the character is an uppercase alphabet (A - Z)
+ * 0, if the character is a lowercase alphabet (a - z)
+ * -1, if the character is not an alphabet
+ * Constraints :
+ * Input can be any character.
+ * Constraints :
+ * Input can be any character.
+ * Input format :
+ * The only line of input contains a single character.
+ * Output format :
+ * The only line of output prints eith1, 0 or -1(as applicable).
+ */
+
+
 import java.util.Scanner;
 
 public class Solution2 {

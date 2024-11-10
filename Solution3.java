dd7e7750f596d9ae1b9ac4023d47b74911bb29f4
Butawantemi <japhetbuta@gmail.com>
2024-11-10 16:50:58 +0300

    java-basic

---
+++ Solution3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Solution3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int basicSalary = sc.nextInt();
+        char grade = sc.next().charAt(0);
+        float hra = 0.2f;
+        float da = 0.5f;
+        float pf = 0.11f;
+
+        if (grade == 'A') {
+            int totalSalary =  (int) Math.floor (basicSalary + hra + 1700 + da - pf);
+            System.out.println(totalSalary);
+        }
+    }
+}

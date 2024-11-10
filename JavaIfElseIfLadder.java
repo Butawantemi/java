b03324d056da2d944ea644886f561a986c701167
Butawantemi <japhetbuta@gmail.com>
2024-11-10 14:27:58 +0300

    java-basic

--- JavaIfElseIfLadder.java
+++ JavaIfElseIfLadder.java
@@ -1,3 +1,17 @@
+
+
+
 public class JavaIfElseIfLadder {
-    
+    public static void main(String[] args) {
+        int number = 30;
+        if(number < 50) {
+            System.out.println("number is less than 50");
+        } else if (number < 40) {
+            System.out.println("number is less than 40");
+        } else if (number < 30) {
+            System.out.println("number is less than 30");
+        } else {
+            System.out.println("number is greater than or equal to 30");
+        }
+    }
 }

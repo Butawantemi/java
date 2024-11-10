7e60e5672969ef39234882b40dfe4dc7f3a81e4e
Butawantemi <japhetbuta@gmail.com>
2024-11-10 14:43:17 +0300

    java-basic

--- JavaSwitchStatement.java
+++ JavaSwitchStatement.java
@@ -3,5 +3,20 @@
  */
 
 public class JavaSwitchStatement {
-    
+    public static void main(String[] args) {
+        int i = 6; 
+        switch (i) {
+            case 0:
+                System.out.println("i is zero");
+                break;
+            case 1:
+                System.out.println("i is one.");
+                break;
+            case 2: 
+                System.out.println("i is two.");
+            default:
+                System.out.println("i is greater than two.");
+                break;
+        }
+    }
 }

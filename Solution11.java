9fb03ce47b65a703a1480ab91f5c33b2a5b850d9
Butawantemi <japhetbuta@gmail.com>
2024-11-11 17:14:50 +0300

    java-basic

---
+++ Solution11.java
@@ -0,0 +1,29 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.Scanner;
+
+class CountPrime {
+	
+	// Write your totalPrime function here
+    public int totalPrime(int s, int e) {
+        int count = 0;
+        for (int i = s; i <= e; i++) {
+            if( i == 2 || i % 2 == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+class Solution {
+	public static void main(String args[]) {
+		
+		CountPrime obj = new CountPrime();
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		int e = sc.nextInt();
+		System.out.println(obj.totalPrime(s, e));
+	}
+}
+

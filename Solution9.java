2ee4242810dfe9e9c7d454c60acc26c1d3afd9a2
Butawantemi <japhetbuta@gmail.com>
2024-11-11 14:57:21 +0300

    java-basic

---
+++ Solution9.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Solution9 {
+    public static List< Integer > printDivisors(int n) {
+        // Write your code here
+       List<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        return divisors;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List< Integer > divisors = printDivisors(n);
+        System.out.println(List.of(divisors));
+        
+    }
+}

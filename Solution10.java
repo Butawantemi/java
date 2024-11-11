d0ca9aac6ff380c7d7d7bd99da2a1332f2e573f2
Butawantemi <japhetbuta@gmail.com>
2024-11-11 16:26:52 +0300

    java-basic

---
+++ Solution10.java
@@ -0,0 +1,23 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution10 {
+	public static int countSetBits(int n) {
+		// Write your code here.
+		int binaryNamber = 0;
+		int i = 1;
+		while (n != 0) {
+		  int mod = n % 2;
+		  n /= 2;
+		  binaryNamber += mod * i;
+		  i *= 10;
+		}
+		return binaryNamber;
+	}
+
+	public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+		 int n = sc.nextInt();
+        
+		 System.out.println(countSetBits(n));
+	}
+}

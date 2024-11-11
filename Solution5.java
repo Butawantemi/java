f2a8cbcde490211ee940d31d632329d2d68885eb
Butawantemi <japhetbuta@gmail.com>
2024-11-11 08:41:50 +0300

    java-basic

---
+++ Solution5.java
@@ -0,0 +1,19 @@
+/*
+ *  Problem statement
+ * Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E),
+ * and Step Size (W), you need to convert all Fahrenheit values from Start to
+ * End at the gap of W into their corresponding Celsius values and print the table.
+ * Note: Print the floor of the Celsius values if they are non-negative else print its ceil value. 
+ */
+
+
+import java.util.* ;
+import java.io.*; 
+class Solution5 {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		
+	}
+}

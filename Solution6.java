2e0dabd62053dc2943f6b1d8bebaede74cf2fadb
Butawantemi <japhetbuta@gmail.com>
2024-11-11 10:00:56 +0300

    java-basic

---
+++ Solution6.java
@@ -0,0 +1,23 @@
+/*
+ *  Problem statement:
+ * Write a program to input an integer N and print the sum of all its
+ * even digits and all its odd digits separately.
+ * Digits mean numbers, not the places! If the given integer is "13245", 
+ * the even digits are 2 and 4, and the odd digits are 1, 3, and 5.
+ */
+
+import java.util.Scanner;
+
+class Solution6 {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        
+		
+	}
+}

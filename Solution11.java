f12558787da999da35bc983d36c8ee28f7b7b282
Butawantemi <japhetbuta@gmail.com>
2024-11-12 10:07:33 +0300

    Adding html

--- Solution11.java
+++ Solution11.java
@@ -5,20 +5,35 @@ import java.util.Scanner;
 class CountPrime {
 	
 	// Write your totalPrime function here
+    public boolean isPrime(int n) {
+        if (n <= 1) return false; 
+        if (n == 2) return true; 
+        if (n % 2 == 0) return false;
+        
+        
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        
+        return true;
+
+    }
+
     public int totalPrime(int s, int e) {
         int count = 0;
         for (int i = s; i <= e; i++) {
-            if( i == 2 || i % 2 == 1) {
+            if(isPrime(i)) {
                 count++;
             }
         }
         return count;
     }
 
+
 }
 
 class Solution11 {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		CountPrime obj = new CountPrime();
 		Scanner sc = new Scanner(System.in);

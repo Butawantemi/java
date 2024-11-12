5ac0cb24d005d4a3986bbf0ef7da2638443dacc1
Butawantemi <japhetbuta@gmail.com>
2024-11-12 13:10:46 +0300

    java-basic

--- Quotient.java
+++ Quotient.java
@@ -8,15 +8,14 @@ public class Quotient {
         int v2 = sc.nextInt();
 
         try {
-            System.out.println( v1 + "/" +v2);
+             int quotient = v1 / v2;
+             System.out.println("Quotient: " + quotient);
+        
         } catch (ArithmeticException e) {
             System.out.println("Number cannot be divided by zero");
         }
 
-
-        int quotient = v1 / v2;
-        
         System.out.println("Program executed successfully!");
-        System.out.println("Quotient: " + quotient);
+        
     }
 }

2a46a820e30f82d12ed3472e3d85eb066e6efdfb
Butawantemi <japhetbuta@gmail.com>
2024-11-12 10:13:48 +0300

    java-basic

---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+    public static void main(String[] args) throws ExceptionInterrupted
+    {
+        Test  t1 = new Test();
+        Test t2 = new Test();
+
+        t1 = null;
+
+        System.gc();
+
+        t2 = null;
+    }
+}

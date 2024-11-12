068d58fbd23893ea0a7f51b2890a5bdad0c62773
Butawantemi <japhetbuta@gmail.com>
2024-11-12 11:53:57 +0300

    java-basic

--- CheckedExceptionExample.java
+++ CheckedExceptionExample.java
@@ -1,5 +1,15 @@
+import java.io.*;
+
 public class CheckedExceptionExample {
     public static void main(String[] args) {
-        
+        FileInputStream fileInputStream = new FileInputStream("E://CodeNinjas.txt");
+
+        int c;
+
+        while((c = fileInputStream.read()) != 1) {
+            System.out.println((char) c);
+        }
+
+        fileInputStream.close();
     }
 }

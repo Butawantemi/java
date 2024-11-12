24d9f22d785b338d8fb3283c5093a22209833628
Butawantemi <japhetbuta@gmail.com>
2024-11-12 13:00:42 +0300

    java-basic

---
+++ Quotient.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Quotient {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int v1 = sc.nextInt();
+        int v2 = sc.nextInt();
+
+
+        int quotient = v1 / v2;
+
+        System.out.println("Quotient: " + quotient);
+    }
+}

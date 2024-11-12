a84fbdca9a595df3427e03bcf3e5dac120e2acd1
Butawantemi <japhetbuta@gmail.com>
2024-11-12 12:11:55 +0300

    java-basic

--- CheckedExceptionExample.java
+++ CheckedExceptionExample.java
@@ -2,14 +2,21 @@ import java.io.*;
 
 public class CheckedExceptionExample {
     public static void main(String[] args) {
-        FileInputStream fileInputStream = new FileInputStream("E://CodeNinjas.txt");
+        FileReader fileReader = null;
 
-        int c;
-
-        while((c = fileInputStream.read()) != 1) {
-            System.out.println((char) c);
+        try {
+            fileReader = new FileReader("CodeNinjas.txt");
+        } catch (FileNotFoundException e1) {
+            System.out.println("File not found in current directory");
         }
 
-        fileInputStream.close();
+        int c;
+        try {
+            while ((c = fileReader.read()) != -1) {
+                System.out.println((char) c);
+            }
+        } catch (Exception e) {
+
+        }
     }
 }

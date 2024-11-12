6d1e9ad2ae9cc031aec167f4951914bedca0dc19
Butawantemi <japhetbuta@gmail.com>
2024-11-12 13:30:00 +0300

    java-basic

--- ExceptionExample.java
+++ ExceptionExample.java
@@ -1,3 +1,19 @@
 public class ExceptionExample {
-    
+    public static void main(String[] args) {
+        int availBalance = 5000;
+        int withDrawAmount = 4500;
+
+        try {
+            if (withDrawAmount > availBalance) {
+                throw new ArithmeticException("Insufficient balance");
+            } else {
+                availBalance -= withDrawAmount;
+                System.out.println("Withdrawal successful");
+                System.out.println("Available balance: " + availBalance);
+            }
+            
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

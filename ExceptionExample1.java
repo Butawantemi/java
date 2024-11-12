9aa5726beae84cf0df2dd1d0dc18f25569163310
Butawantemi <japhetbuta@gmail.com>
2024-11-12 13:54:39 +0300

    java-basic

--- ExceptionExample1.java
+++ ExceptionExample1.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
+
 public class ExceptionExample1 {
-    public static void main(String[] args) {
-        
+    public void validatePinCode(int pinCode) throws InvalidPinCodeException {
+        if (!(pinCode >= 100000 || pinCode <= 999999)) {
+            throw new InvalidPinCodeException("Pin code must be a 6-digits number");
+        }
+        System.out.println(pinCode);
+    
+    }
+
+    public static void main(String[] args) throws InvalidPinCodeException {
+        ExceptionExample1 obj = new ExceptionExample1();
+        obj.validatePinCode(123334);
     }
 }

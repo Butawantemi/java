284429ef1cc8bd1f7776cc68e6d8e51e21c6b9c8
Butawantemi <japhetbuta@gmail.com>
2024-11-15 13:00:10 +0300

    java-basic

--- JavaArray.java
+++ JavaArray.java
@@ -5,9 +5,10 @@ public class JavaArray {
         /* for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         } */
-
+        int sum = 0;
         for (int number : arr) {
-            System.out.println(number);
+            sum += number;
+            System.out.println(sum);
         }
     }
 }

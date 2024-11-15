ad9f3c621096f60cd3387ff9628f2db990a25e28
Butawantemi <japhetbuta@gmail.com>
2024-11-15 13:18:46 +0300

    java-data-structure

--- MultidimensionalArray.java
+++ MultidimensionalArray.java
@@ -1,5 +1,15 @@
 public class MultidimensionalArray {
     public static void main(String[] args) {
-        
+        int[][] arr = {
+            {1, 3, 5, 6, 7},
+            {478, 89, 90},
+            {5,6,7}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.err.println(i + " " + j);
+            }
+        }
     }
 }

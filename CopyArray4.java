6a1365a827829ffe2d52f8a70ae237c7c2ce755a
Butawantemi <japhetbuta@gmail.com>
2024-11-15 14:10:34 +0300

    java-data-structure

--- CopyArray4.java
+++ CopyArray4.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class CopyArray4 {
     public static void main(String[] args) {
-        
+        int[] numbers = {1, 3, 5, 7, 9, 10};
+
+        int[] copyNumbers = new int[numbers.length];
+
+        copyNumbers = Arrays.copyOfRange(numbers, 0, numbers.length);
+
+        System.out.println(Arrays.toString(copyNumbers));
     }
 }

b35aa180a3937991d8c1516611b72b08df6afba5
Butawantemi <japhetbuta@gmail.com>
2024-11-15 13:49:35 +0300

    java-data-structure

--- CopyArray2.java
+++ CopyArray2.java
@@ -1,11 +1,13 @@
-
+/*
+ * Copy an array is loop costruct.
+ */
 
 import java.util.*;
 
 public class CopyArray2 {
     public static void main(String[] args) {
        int[] source = {1, 2, 3, 4, 5};
-       int[] destination =new int[5]; 
+       int[] destination = new int[5]; 
 
 
        for (int i = 0; i < source.length; ++i) {

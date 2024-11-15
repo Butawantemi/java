93cf71f2d349dcae42f30fa35ee5ddfb0e31b0ff
Butawantemi <japhetbuta@gmail.com>
2024-11-15 14:00:58 +0300

    java-data-structure

--- CopyArray3.java
+++ CopyArray3.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class CopyArray3 {
     public static void main(String[] args) {
-        
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+         
+        int[] copyNumbers = new int[numbers.length];
+
+        System.arraycopy(numbers, 0, copyNumbers, 0, numbers.length);
+
+        System.out.println(Arrays.toString(copyNumbers));
     }
 }

8f106ee89cfd71451af8d057efdb5179eea0564a
Butawantemi <japhetbuta@gmail.com>
2024-11-15 12:58:03 +0300

    java-basic

--- JavaArray.java
+++ JavaArray.java
@@ -2,8 +2,12 @@ public class JavaArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-        for (int i = 0; i < arr.length; i++) {
+        /* for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
+        } */
+
+        for (int number : arr) {
+            System.out.println(number);
         }
     }
 }

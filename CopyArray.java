030636523dc9b969868dc3d58d84456c9439ddda
Butawantemi <japhetbuta@gmail.com>
2024-11-15 13:35:25 +0300

    java-data-structure

--- CopyArray.java
+++ CopyArray.java
@@ -1,5 +1,11 @@
 public class CopyArray {
     public static void main(String[] args) {
-        
+        int[] numbers = { 1, 2, 3, 4, 5};
+        int[] positiveNumbers = numbers;
+        numbers[0] = 3;
+
+
+       System.out.println(positiveNumbers);
+
     }
 }

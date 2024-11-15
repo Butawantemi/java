e9c20ffb0a69ca86dc43fdcb789e8dfa1de50ca2
Butawantemi <japhetbuta@gmail.com>
2024-11-15 13:47:24 +0300

    java-data-structure

--- CopyArray2.java
+++ CopyArray2.java
@@ -1,5 +1,17 @@
+
+
+import java.util.*;
+
 public class CopyArray2 {
     public static void main(String[] args) {
-        
+       int[] source = {1, 2, 3, 4, 5};
+       int[] destination =new int[5]; 
+
+
+       for (int i = 0; i < source.length; ++i) {
+        destination[i] = source[i];
+       }
+
+       System.out.println(Arrays.toString(destination));
     }
 }

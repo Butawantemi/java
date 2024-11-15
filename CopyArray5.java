d6b1d6f2de77fb020ad198d942e818848261e997
Butawantemi <japhetbuta@gmail.com>
2024-11-15 15:21:48 +0300

    java-data-structure

--- CopyArray5.java
+++ CopyArray5.java
@@ -1,5 +1,19 @@
 public class CopyArray5 {
     public static void main(String[] args) {
-        
+        int[][] source = {
+            {1, 2, 3},
+            {5, 6, 8},
+            {0, 8}
+        };
+
+        int[][] destination = new int[source.length][];
+
+        for (int i = 0; i < source.length; ++i) {
+            destination[i] = new int[source.length];
+
+            for (int j = 0; j < source[i].length; ++j) {
+                destination[i][j] = source[i][j];
+            }
+        }
     }
 }

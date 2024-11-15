951884197c6ec0943649bf6ec8b37eb11910f5c3
Butawantemi <japhetbuta@gmail.com>
2024-11-15 15:28:03 +0300

    java-data-structure

--- CopyArry6.java
+++ CopyArry6.java
@@ -1,5 +1,12 @@
 public class CopyArry6 {
     public static void main(String[] args) {
-        
+        int[][] source = {
+            {1, 2, 3},
+            {5, 6, 8},
+            {0, 8}
+        };
+
+        int[][] destination = new int[source.length][];
+
     }
 }
